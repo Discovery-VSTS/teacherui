3813655eb221e454cc78beeb7118ab6bdda97b81
Ricard Gascons <gascons1995@gmail.com>
2017-03-14 21:24:28 +0000

    Use inheritance and different views extending from base.html

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'index/$', views.index),
+    url(r'100-point/$', views.tab_100_points),
+    url(r'effort-analysis', views.tab_effort_analysis),
+    url(r'team-projects', views.tab_team_projects),
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,18 @@ from django.template.loader import get_template
 def index(request):
     html = get_template('base.html')
     return HttpResponse(html.render())
+
+
+def tab_100_points(request):
+    template = get_template('tabs/tab_100_points.html')
+    return HttpResponse(template.render())
+
+
+def tab_effort_analysis(request):
+    template = get_template('tabs/tab_effort_analysis.html')
+    return HttpResponse(template.render())
+
+
+def tab_team_projects(request):
+    template = get_template('tabs/tab_team_projects.html')
+    return HttpResponse(template.render())
