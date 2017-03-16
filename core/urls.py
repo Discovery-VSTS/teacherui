68221f000d3a7c0671c1353c53b16eeefaeea1dd
Yichen Lu <litash529@gmail.com>
2017-03-16 15:58:04 +0000

    Control nav bar active item by django view

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'$', views.tab_100_points),
     url(r'login/$', views.login),
     url(r'index/$', views.index),
     url(r'100-point/$', views.tab_100_points),
