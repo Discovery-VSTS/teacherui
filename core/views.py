9230a0e5f03feb87ded5155da905888c1b1f200f
Ricard Gascons <gascons1995@gmail.com>
2017-03-17 00:27:10 +0000

    Populate table total points of a team

--- core/views.py
+++ core/views.py
@@ -1,29 +1,32 @@
 from django.http import HttpResponse
 from django.template import Context
 from django.template.loader import get_template
+import requests
 
 
 def login(request):
     template = get_template('login/login.html')
     return HttpResponse(template.render())
 
-liveStatus = dict(
-    navStatus={
-        "hundredPt": "",
-        "codemetrics": ""
-    }
-)
 
 def tab_100_points(request):
     template = get_template('tabs/tab_100_points.html')
-    liveStatus['navStatus']['hundredPt'] = "active"
-    liveStatus['navStatus']['codemetrics'] = ""
-    return HttpResponse(template.render(Context(liveStatus)))
+    BASE_URL = 'http://discovery-100p.azurewebsites.net/{}{}'
+    TEAM = request.GET.get('team', '')
+    r = requests.get(BASE_URL.format('v1/team/points/', '?instance_id=%s' % TEAM))
+    total_points_team = r.json()
+    return HttpResponse(template.render(Context(
+        {
+            'total_points_team': total_points_team
+        }
+    )))
 
 
 def tab_codemetrics(request):
     template = get_template('tabs/tab_codemetrics.html')
-    liveStatus['navStatus']['hundredPt'] = ""
-    liveStatus['navStatus']['codemetrics'] = "active"
-    return HttpResponse(template.render(Context(liveStatus)))
+    return HttpResponse(template.render(Context(
+        {
+
+        }
+    )))
 
